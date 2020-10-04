835d668cb903f09be12fdd3e6233b749ff6475d5
nairitprachanda <nairitprachanda@gmail.com>
2020-10-04 11:52:45 +0530

    [nairit] Refactor: Welcome to Line Comparision

---
+++ line comparision/line comparision/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace line_comparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparision Computation Program");
+        }
+    }
+}
